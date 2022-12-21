7bf9084b4d17a8d3ae6833d444df3ad2126d1d50
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-21 10:50:15 +0200

    Using regular expressions to find content

--- src/app/pipes/highlight-text.pipe.ts
+++ src/app/pipes/highlight-text.pipe.ts
@@ -4,7 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'highlightText',
 })
 export class HighlightTextPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return value;
+  transform(value: string, filter: string): string {
+    if (filter.length == 0) {
+      return value;
+    }
+    const search = new RegExp(filter, 'ig');
+    return value.replace(search, 'x');
   }
 }
