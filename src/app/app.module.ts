0b2cc5e413a15ad03f9b1ed770c560ddf7a86eaa
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-21 10:44:30 +0200

    Adding a custom angular pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { ErrorMetadataService } from './services/error-metadata.service';
+import { HighlightTextPipe } from './pipes/highlight-text.pipe';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent],
+  declarations: [AppComponent, UserListComponent, HighlightTextPipe],
   imports: [BrowserModule, AppRoutingModule],
   providers: [{ provide: ErrorHandler, useClass: ErrorMetadataService }],
   bootstrap: [AppComponent],
